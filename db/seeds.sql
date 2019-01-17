86ebac8031d6072d243cd62f0522a3b55add5c1f
Bhumi Shah <bshah11@outlook.com>
2019-01-17 01:58:22 -0700

    devoured is coming as null and css not working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Quinoa pattie burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg club burger', true);
