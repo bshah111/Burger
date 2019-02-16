1fbb46cb87103d10a059d3165b3d2d93db0e6dbd
Bhumi Shah <bshah11@outlook.com>
2019-02-16 13:51:12 -0700

    adding express handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,1 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Quinoa pattie burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veg club burger', true);
+n
