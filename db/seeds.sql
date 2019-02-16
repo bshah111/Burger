4a256a987a50f4cfdd62af8c73e554aa7db725c7
Bhumi Shah <bshah11@outlook.com>
2019-02-16 14:24:47 -0700

    added process.env.port

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-n
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Quinoa pattie burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg club burger', true);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs = require("express-handlebars")
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT = 3000;
+var PORT = process.env.PORT ||3000;
 
 var app = express();
 
